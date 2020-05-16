d13ee69011389672e647b96b13d994b49dcbc449
Austin Doody <AustinDoody20@gmail.com>
2020-05-16 09:14:08 -0400

    Initial Commit

---
+++ Source/S05_TestingGroundEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class S05_TestingGroundEditorTarget : TargetRules
+{
+	public S05_TestingGroundEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("S05_TestingGround");
+	}
+}
